da853b8b8089ed60e60351898642b8239819d3ff
Keith Casko <kcasko@wgu.edu>
2025-10-22 20:15:56 -0400

    Change Airtable table name from 'Items' to 'Games'

--- functions/api/games.js
+++ functions/api/games.js
@@ -11,7 +11,7 @@ export async function onRequestGet(context) {
   const BASE_ID = env.AIRTABLE_BASE_ID;
   
   // This MUST match your Airtable table name (it's case-sensitive)
-  const TABLE_NAME = "1. Items"; 
+  const TABLE_NAME = "1. Games"; 
 
   // This new line wraps TABLE_NAME in encodeURIComponent()
   const url = `https://api.airtable.com/v0/${BASE_ID}/${encodeURIComponent(TABLE_NAME)}`;
